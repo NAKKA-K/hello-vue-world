b920f1096860bd34a24544119b3166945b45c507
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-25 16:19:46 +0000

    Add v-if msg

--- src/components/Scene1.vue
+++ src/components/Scene1.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="hello">
     <h1 class="page-title">{{ page }}</h1>
-    <h1>{{ msg }}</h1>
+    <h1 v-if="msg.length > 0">
+      {{ msg }}
+    </h1>
+    <h1 v-else>
+      [! No Text]
+    </h1>
     <input type="text" v-model="msg">
 
     <ul class="router">
