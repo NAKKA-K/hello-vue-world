2d1bb3a3ddbc1d6cdcc68bf94e38cd87ac64ca2a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-26 14:26:02 +0000

    Update split component to header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div class="header">
-      <img class="img" src="./assets/logo.png">
-      <p class="title">Hello <span>Vue</span> WORLD!</p>
-    </div>
+    <Header></Header>
 
     <!-- それぞれのページを読み込み -->
     <div class="container">
@@ -23,8 +20,13 @@
 </template>
 
 <script>
+import Header from './components/header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
@@ -41,28 +43,6 @@ body {
   text-align: center;
   color: #2c3e50;
   margin-top: 10px;
-}
-
-.header {
-  height: 120px;
-  background-color: #EEE;
-  border-bottom: 8px solid #CCC;
-  .img {
-    float: left;
-    width: 6%;
-    padding: 10px 30px;
-  }
-  .title {
-    float: left;
-    font-size: 1.8rem;
-    padding: 5px;
-    margin-top: 20px;
-    font-family: serif;
-    span {
-      font-size: 2.5rem;
-      text-shadow: 1px 1px 3px #000;
-    }
-  }
 }
 
 .footer {
